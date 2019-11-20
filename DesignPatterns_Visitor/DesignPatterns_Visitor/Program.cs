be204144c04284193f3aa20a7e87ae97fa7de0df
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-20 12:48:20 +0300

    Initial commit

---
+++ DesignPatterns_Visitor/DesignPatterns_Visitor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Visitor!");
+        }
+    }
+}
